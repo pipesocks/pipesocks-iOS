f4c0fdf6a1b7afed64b2d07621a73039bc7ea5a6
yvbbrjdr <xyyqzy@hotmail.com>
2017-02-01 10:42:10 +0800

    UI Controls

--- pipesocks/pipesocks/ViewController.swift
+++ pipesocks/pipesocks/ViewController.swift
@@ -10,16 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var remoteHost: UITextField!
+    @IBOutlet weak var remotePort: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var startButton: UIButton!
+
+    let ver=Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString")!
+    let notFilled=UIAlertController.init(title: "Error", message: "Fill in the blanks!", preferredStyle: UIAlertControllerStyle.alert)
+    let OKButton=UIAlertAction.init(title: "OK", style: UIAlertActionStyle.default, handler: nil)
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        notFilled.addAction(OKButton)
+        titleLabel.text="pipesocks \(ver)"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func remoteHostDone() {
+        remotePort.becomeFirstResponder()
+    }
 
+    @IBAction func remotePortDone() {
+        password.becomeFirstResponder()
+    }
+
+    @IBAction func passwordDone() {
+        password.resignFirstResponder()
+    }
+    
+    @IBAction func startClicked() {
+        if startButton.titleLabel?.text=="Start" {
+            if !(remoteHost.text==""||remotePort.text=="") {
+                startButton.setTitle("Stop", for: UIControlState.normal)
+                remoteHost.isEnabled=false
+                remotePort.isEnabled=false
+                password.isEnabled=false
+                titleLabel.text="Enjoy!"
+            } else {
+                present(notFilled, animated: true, completion: nil)
+            }
+        } else if startButton.titleLabel?.text=="Stop" {
+            startButton.setTitle("Start", for: UIControlState.normal)
+            remoteHost.isEnabled=true
+            remotePort.isEnabled=true
+            password.isEnabled=true
+            titleLabel.text="pipesocks \(ver)"
+        }
+    }
 }
 
