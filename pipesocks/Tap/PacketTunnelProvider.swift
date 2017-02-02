81a5fae4ea81b52f22f9d41bdf7d3411ee48ab93
yvbbrjdr <xyyqzy@hotmail.com>
2017-02-02 11:23:17 +0800

    Added Tap

---
+++ pipesocks/Tap/PacketTunnelProvider.swift
@@ -0,0 +1,13 @@
+//
+//  PacketTunnelProvider.swift
+//  Tap
+//
+//  Created by Jerry Zhou on 02/2.
+//  Copyright © 2017 yvbbrjdr. All rights reserved.
+//
+
+import NetworkExtension
+
+class PacketTunnelProvider: NEPacketTunnelProvider {
+
+}
