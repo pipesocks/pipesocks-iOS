24996d2e9e27cb52b0c7799a3b245bdc4abe046d
yvbbrjdr <xyyqzy@hotmail.com>
2017-03-02 22:29:13 +0800

    pipesocks key exchange is working! congrats

--- pipesocks/Tap/PipesocksAdapter.swift
+++ pipesocks/Tap/PipesocksAdapter.swift
@@ -8,18 +8,48 @@
 
 import Foundation
 import NEKit
+import Sodium
 
 class PipesocksAdapter: AdapterSocket {
+
+    enum PipesocksAdapterStatus {
+        case idle,
+        connecting,
+        connected,
+        forwarding,
+        disconnected
+    }
+    var internalStatus:PipesocksAdapterStatus = .idle
 
     var remoteHost:String=""
     var remotePort:UInt16=0
     var password:String=""
+    var secretKey:Data?
+    var localPubKey:Data?
+    var localPriKey:Data?
+    var remotePubKey:Data?
 
     init(remoteHost: String, remotePort: UInt16, password: String) {
+        super.init()
         self.remoteHost=remoteHost
         self.remotePort=remotePort
         self.password=password
-        super.init()
+        localPubKey=Data.init(count: Int.init(crypto_box_PUBLICKEYBYTES))
+        localPriKey=Data.init(count: Int.init(crypto_box_SECRETKEYBYTES))
+        if (sodium_init() == -1) {
+            exit(1)
+        }
+        localPubKey!.withUnsafeMutableBytes { (localPubKey: UnsafeMutablePointer<UInt8>) -> Void in
+            localPriKey!.withUnsafeMutableBytes({ (localPriKey: UnsafeMutablePointer<UInt8>) -> Void in
+                crypto_box_keypair(localPubKey, localPriKey)
+            })
+        }
+        secretKey=password.data(using: String.Encoding.ascii)
+        if (secretKey!.count>=Int.init(crypto_secretbox_KEYBYTES)) {
+            secretKey=secretKey!.prefix(Int.init(crypto_secretbox_KEYBYTES)).base
+        } else {
+            secretKey!.append(Data.init(bytes: Array<UInt8>.init(repeating: UInt8.init(0x98), count: Int.init(crypto_secretbox_KEYBYTES)-secretKey!.count)))
+        }
     }
 
     public override func openSocketWith(session: ConnectSession) {
@@ -27,12 +57,15 @@ class PipesocksAdapter: AdapterSocket {
         guard !isCancelled else {
             return
         }
+        internalStatus = .connecting
         try! socket.connectTo(host: remoteHost, port: Int.init(remotePort), enableTLS: false, tlsSettings: nil)
     }
 
     public override func didConnectWith(socket: RawTCPSocketProtocol) {
         super.didConnectWith(socket: socket)
-        socket.write(data: password.data(using: String.Encoding.ascii)!)
+        internalStatus = .connected
+        sendPubKey()
+        socket.readData()
     }
 
     public override func didRead(data: Data, from socket: RawTCPSocketProtocol) {
@@ -41,5 +74,125 @@ class PipesocksAdapter: AdapterSocket {
 
     override open func didWrite(data: Data?, by socket: RawTCPSocketProtocol) {
         super.didWrite(data: data, by: socket)
+    }
+
+    override func write(data: Data) {
+        sendUnencrypted(data: data)
+    }
+
+    func write(rawData: Data) {
+        super.write(data: rawData)
+    }
+
+    func sendEncrypted(data: Data) {
+        let l:UInt32=crypto_secretbox_MACBYTES+4+crypto_secretbox_NONCEBYTES+UInt32.init(data.count)
+        let prefix:Data=Data.init(bytes: [UInt8.init(l>>24&0xff),UInt8.init(l>>16&0xff),UInt8.init(l>>8&0xff),UInt8.init(l&0xff)])
+        var ret:Data=secretEncrypt(data: prefix)
+        ret.append(data)
+        write(rawData: ret)
+    }
+
+    func sendPubKey() {
+        let l:Int=Int.init(randombytes_uniform(900))
+        var garbage:Data=Data.init(count: l)
+        garbage.withUnsafeMutableBytes { (garbage: UnsafeMutablePointer<UInt8>) -> Void in
+            randombytes_buf(garbage, l)
+        }
+        garbage.append(localPubKey!)
+        sendEncrypted(data: secretEncrypt(data: garbage))
+    }
+
+    func sendUnencrypted(data: Data) {
+        sendEncrypted(data: publicEncrypt(data: data))
+    }
+
+    func publicEncrypt(data: Data) -> Data {
+        var ret:Data=Data.init(count: Int.init(crypto_box_MACBYTES)+data.count)
+        var nonce:Data=Data.init(count: Int.init(crypto_box_NONCEBYTES))
+        nonce.withUnsafeMutableBytes { (nonce: UnsafeMutablePointer<UInt8>) -> Void in
+            randombytes_buf(nonce, Int.init(crypto_box_NONCEBYTES))
+        }
+        let result:Int32=ret.withUnsafeMutableBytes { (ret: UnsafeMutablePointer<UInt8>) -> Int32 in
+            return data.withUnsafeBytes({ (dataPtr: UnsafePointer<UInt8>) -> Int32 in
+                return nonce.withUnsafeBytes({ (nonce: UnsafePointer<UInt8>) -> Int32 in
+                    return remotePubKey!.withUnsafeBytes({ (remotePubKey: UnsafePointer<UInt8>) -> Int32 in
+                        return localPriKey!.withUnsafeBytes({ (localPriKey: UnsafePointer<UInt8>) -> Int32 in
+                            return crypto_box_easy(ret, dataPtr, UInt64.init(data.count), nonce, remotePubKey, localPriKey)
+                        })
+                    })
+                })
+            })
+        }
+        if (result==0) {
+            ret.append(nonce)
+            return ret
+        }
+        disconnect()
+        return Data.init()
+    }
+
+    func publicDecrypt(data: Data) -> Data {
+        var ret:Data=Data.init(count: data.count-Int.init(crypto_box_MACBYTES)-Int.init(crypto_box_NONCEBYTES))
+        let encrypted:Data=data.prefix(data.count-Int.init(crypto_box_NONCEBYTES)).base
+        let nonce:Data=data.suffix(Int.init(crypto_box_NONCEBYTES)).base
+        let result:Int32=ret.withUnsafeMutableBytes { (ret: UnsafeMutablePointer<UInt8>) -> Int32 in
+            return encrypted.withUnsafeBytes({ (encryptedPtr: UnsafePointer<UInt8>) -> Int32 in
+                return nonce.withUnsafeBytes({ (nonce: UnsafePointer<UInt8>) -> Int32 in
+                    return remotePubKey!.withUnsafeBytes({ (remotePubKey: UnsafePointer<UInt8>) -> Int32 in
+                        return localPriKey!.withUnsafeBytes({ (localPriKey: UnsafePointer<UInt8>) -> Int32 in
+                            return crypto_box_open_easy(ret, encryptedPtr, UInt64.init(encrypted.count), nonce, remotePubKey, localPriKey)
+                        })
+                    })
+                })
+            })
+        }
+        if (result==0) {
+            return ret
+        }
+        disconnect()
+        return Data.init()
+    }
+
+    func secretEncrypt(data: Data) -> Data {
+        var ret:Data=Data.init(count: Int.init(crypto_secretbox_MACBYTES)+data.count)
+        var nonce:Data=Data.init(count: Int.init(crypto_secretbox_NONCEBYTES))
+        nonce.withUnsafeMutableBytes { (nonce: UnsafeMutablePointer<UInt8>) -> Void in
+            randombytes_buf(nonce, Int.init(crypto_secretbox_NONCEBYTES))
+        }
+        let result:Int32=ret.withUnsafeMutableBytes { (ret: UnsafeMutablePointer<UInt8>) -> Int32 in
+            return data.withUnsafeBytes({ (dataPtr: UnsafePointer<UInt8>) -> Int32 in
+                return nonce.withUnsafeBytes({ (nonce: UnsafePointer<UInt8>) -> Int32 in
+                    return secretKey!.withUnsafeBytes({ (secretKey: UnsafePointer<UInt8>) -> Int32 in
+                        return crypto_secretbox_easy(ret, dataPtr, UInt64.init(data.count), nonce, secretKey)
+                    })
+                })
+            })
+        }
+        if (result==0) {
+            ret.append(nonce)
+            return ret
+        }
+        disconnect()
+        return Data.init()
+    }
+
+    func secretDecrypt(data: Data) -> Data {
+        var ret:Data=Data.init(count: data.count-Int.init(crypto_secretbox_MACBYTES)-Int.init(crypto_secretbox_NONCEBYTES))
+        let encrypted:Data=data.prefix(data.count-Int.init(crypto_secretbox_NONCEBYTES)).base
+        let nonce:Data=data.suffix(Int.init(crypto_secretbox_NONCEBYTES)).base
+        let result:Int32=ret.withUnsafeMutableBytes { (ret: UnsafeMutablePointer<UInt8>) -> Int32 in
+            return encrypted.withUnsafeBytes({ (encryptedPtr: UnsafePointer<UInt8>) -> Int32 in
+                return nonce.withUnsafeBytes({ (nonce: UnsafePointer<UInt8>) -> Int32 in
+                    return secretKey!.withUnsafeBytes({ (secretKey: UnsafePointer<UInt8>) -> Int32 in
+                        return crypto_secretbox_open_easy(ret, encryptedPtr, UInt64.init(encrypted.count), nonce, secretKey)
+                    })
+                })
+            })
+        }
+        if (result==0) {
+            return ret
+        }
+        disconnect()
+        return Data.init()
     }
 }
